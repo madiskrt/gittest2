6304c0b58c401d19c9ffc025224c8b802ad6191c
Madis Kaaret <madiskrt@gmail.com>
2021-03-11 21:44:42 +0200

    message

---
+++ app/src/main/java/com/example/a210311_gittest2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a210311_gittest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //some text here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
